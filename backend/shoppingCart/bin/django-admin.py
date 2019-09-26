e68274b0ff8cf8216902afbef2253b98b5168b37
vishnupriyaRamanathan <rvishnu97@gmail.com>
2019-09-27 00:10:15 +0530

    Initial commit

---
+++ backend/shoppingCart/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vishnupriya/Documents/9th sem/webtech/shopping-cart/backend/shoppingCart/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
